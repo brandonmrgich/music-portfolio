4a9c45437eac093776683aad1c480e8b83e3405c
brandonmrgich <58794109+brandonmrgich@users.noreply.github.com>
2024-12-07 14:04:57 -0500

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 // Import API routes
-//const audioRoutes = require('./routes/audio');
 const emailRoutes = require('./routes/email');
-//const uploadRoutes = require('./routes/upload');
 const hbRoutes = require('./routes/heartbeat');
 
 const app = express();
 const port = 5000;
+
+// CORS configuration
+const corsOptions = {
+    origin: 'https://brandonmrgich.com', // Allow requests from this frontend domain
+    methods: 'GET,POST,PUT,DELETE', // Allow only these HTTP methods
+    allowedHeaders: 'Content-Type', // Allow only specific headers
+    credentials: true, // Allow cookies, if necessary
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(bodyParser.json()); // Parse JSON bodies for POST requests
 // TODO: Auth
 
 // Use the routes
-//app.use('/api/audio', audioRoutes);  // All audio-related endpoints will be prefixed with /api/audio
 app.use('/api/send-email', emailRoutes); // Email-related endpoints
-//app.use('/api/upload', uploadRoutes);  // File upload routes
 app.use('/api/heartbeat', hbRoutes);
-
-// Serve the frontend build files (Currently nothing frontend being served)
-//app.use(express.static(path.join(__dirname, '../build')));
-
-// Catch-all for frontend routing (if needed)
-//app.get('*', (req, res) => {
-//    res.sendFile(path.join(__dirname, '../build/index.html'));
-//});
 
 // Start the server
 app.listen(port, () => {
