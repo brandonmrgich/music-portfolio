c2a876761e39134f6d83427b7ffe02688807feac
brandonmrgich <58794109+brandonmrgich@users.noreply.github.com>
2025-06-02 18:03:51 -0400

    Fixed s3 instance

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -3,6 +3,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
     origin: isProduction ? 'https://brandonmrgich.com' : 'http://localhost:3000', // Use localhost in dev
     methods: 'GET,POST,PUT,DELETE', // Allow only these HTTP methods
-    allowedHeaders: 'Content-Type', // Allow only specific headers
+    //allowedHeaders: 'Content-Type', // Allow only specific headers
     credentials: true, // Allow cookies, if necessary
 };
--- backend/middleware/s3Middleware.js
+++ backend/middleware/s3Middleware.js
@@ -1,7 +1,7 @@
 const { getS3Instance } = require('../utils/S3Helper.js');
 
 const s3Middleware = (req, res, next) => {
-    if (req.isDev) req.s3 = getS3Instance(); // Attach the singleton S3 instance to the request
+    req.s3 = getS3Instance(); // Attach the singleton S3 instance to the request
     next();
 };
 
